fbf384e03e8eb309f2c465310e46b9b78787b833
Yaroslav Mironov <ya.mironov14@gmail.com>
2023-04-26 18:53:51 +0300

    теперь можно парсить по имени, тексту документа, организациям

--- pravo_gov/pravo_gov/spiders/utils/startlinkcreator.py
+++ pravo_gov/pravo_gov/spiders/utils/startlinkcreator.py
@@ -12,34 +12,60 @@ class StartLinkCreator:
         self.base_url = 'http://pravo.gov.ru/proxy/ips/'
         self.region_code = self.get_region_code(
             kwargs.get('region'))  # Возвращает список регионов для подключения БПА, кодировать через /, bpas
-        self.org_code = ''  # self.get_organization_code(kwargs.get('organization')) #Список организаций, кодировать через ;, v6, v6type=1
+        self.org_code = self.get_organization_code(kwargs.get('organization')) #Список организаций, кодировать через ;, v6, v6type=1
         self.from_date = kwargs.get('from_date') or '26.10.1917'
         self.to_date = kwargs.get('to_date') or '24.05.2023'
-        self.name = kwargs.get('name') or ''  # a1
+        self.name = kwargs.get('name') or ''  #
         self.text = kwargs.get('text') or ''
 
     def get_start_link(self):
-        base_params = {'bpas': '', 'v3': '', 'v3type': '1', 'v3value': '', 'a6': '', 'a6type': '1', 'a6value': '',
-                       'a15': '', 'a15type': '1', 'a15value': '', 'a7type': '3', 'a7from': '', 'a7to': '', 'a7date': '',
-                       'a8': '', 'a8type': '1', 'a1': '', 'a0': '', 'a16': '', 'a16type': '1', 'a16value': '',
-                       'a17': '',
-                       'a17type': '1', 'a17value': '', 'a4': '', 'a4type': '1', 'a4value': '', 'a23': '',
-                       'a23type': '1',
-                       'a23value': '', 'textpres': '', 'sort': '7', 'x': '48', 'y': '8', 'lstsize': '200', 'start': '0'}
+        base_params = {
+            'bpas': '',
+            'v3': '',
+            'v3type': '1',
+            'v3value': '',
+            'a6': '',
+            'a6type': '1',
+            'a6value': '',
+            'a7type': '3',
+            'a7from': '',
+            'a7to': '',
+            'a7date': '',
+            'a8': '',
+            'a8type': '1',
+            'a1': '',
+            'a0': '',
+            'a4': '',
+            'a4type': '1',
+            'a4value': '',
+            'a23': '',
+            'a23type': '1',
+            'a23value': '',
+            'textpres': '',
+            'sort': '7',
+            'x': '48',
+            'y': '8',
+            'lstsize': '200',
+            'start': '0'
+        }
 
-        # base_params = {'bpas':'', 'a3type': '1', 'a6type': '1', 'a15type': '1', 'a7type': '4', 'a7from': '', 'a7to': '', 'a8type': '1', 'a0': '', 'a16type': '1', 'a17type': '1', 'a4type': '1', 'a23type': '1', 'sort': '7','lstsize': '200', 'start': '0' }
+        user_params = {
+            'a7type': '4',
+            'bpas': '/'.join(self.region_code),
+            'a6': ';'.join(self.org_code) or '',
+            'a6value': ';'.join(self.org_code),
+            'a7from': self.from_date,
+            'a7to': self.to_date,
+            'a0': self.text,
+            'a1': self.name,
+        }
 
-        user_params = {'a7type': '4',
-                       'bpas': '/'.join(self.region_code).encode('cp1251'),
-                       "a6": ';'.join(self.org_code).encode('cp1251') or '',
-                       'a6value': ';'.join(self.org_code).encode('cp1251'),
-                       'a7from': self.from_date,
-                       'a7to': self.to_date,
-                       'a0': self.text,
-                       'a1': self.name,
-                       }
+        # Обновляем значения базовых параметров данными пользователя
         base_params.update(user_params)
-        encoded_params = urllib.parse.urlencode(base_params, encoding='cp1251')
+
+        # Кодируем параметры в нужной кодировке
+        encoded_params = urllib.parse.urlencode(base_params, encoding='cp1251', quote_via=urllib.parse.quote)
+
         url = self.base_url + '?searchlist=&' + encoded_params
         return url
 
@@ -73,10 +99,7 @@ class StartLinkCreator:
         with open(Path(__file__).parent / 'gov_bodies_n_their_codes.json', encoding='utf-8') as f:
             codes = json.load(f)
             if not org:
-                orgs = []
-                for org_code in codes.values():
-                    orgs.append(org_code)
-                return orgs
+                return ''
             elif isinstance(org, str) and len(org.split(',')) > 1:
                 orgs = []
                 for organization in org.split(','):
@@ -94,5 +117,3 @@ class StartLinkCreator:
                         f'''{org} направильно указан регион. допустимые значения {list(codes.keys())}''')
 
 
-t = StartLinkCreator()
-print(t.get_start_link())
